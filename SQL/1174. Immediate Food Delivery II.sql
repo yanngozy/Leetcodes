a311e4264c67da98d09b44a376bcc160919bb6a8
Yannick Gonondo <104365606+yanngozy@users.noreply.github.com>
2026-05-07 01:20:40 +0200

    Add query to calculate immediate delivery percentage

---
+++ SQL/1174. Immediate Food Delivery II.sql
@@ -0,0 +1,10 @@
+SELECT 
+    ROUND(
+        SUM(order_date = customer_pref_delivery_date) * 100.0 / COUNT(*), 2
+        ) AS immediate_percentage
+FROM Delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM Delivery
+    GROUP BY customer_id
+);
